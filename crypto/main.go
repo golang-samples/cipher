f9c0ad901054a126e15e455244402355f9e4067b
mattn <mattn.jp@gmail.com>
2013-02-20 10:43:48 +0900

    Add crypto

---
+++ crypto/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"crypto/des"
+	"crypto/cipher"
+	"fmt"
+	"math/rand"
+)
+
+func makeRand(size int) []byte {
+	r := make([]byte, size)
+	for n := 0; n < len(r); n++ {
+		r[n] = uint8(rand.Int() % 256)
+	}
+	return r
+}
+
+func main() {
+	in := []byte("helloworld")
+	key := []byte("love_go!")
+
+	ci, _ := des.NewCipher(key[0:8])
+	iv := makeRand(8)
+
+	enc := cipher.NewCBCEncrypter(ci, iv)
+	cin := make([]byte, int(len(in)/des.BlockSize)*des.BlockSize+des.BlockSize)
+	out := make([]byte, len(cin))
+	enc.CryptBlocks(out, cin)
+	fmt.Printf("%X", iv)
+}
